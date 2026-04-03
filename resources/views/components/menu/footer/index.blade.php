ada34b7b747c5f1a2cf9373382a3815ecc9cf8d9
voltexman <voltexman@gmail.com>
2026-04-03 11:32:53 +0300

    допрацювання

--- resources/views/components/menu/footer/index.blade.php
+++ resources/views/components/menu/footer/index.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class('flex flex-row lg:flex-col gap-1.5 lg:gap-2.5') }}>
+<div {{ $attributes->class('flex flex-row lg:flex-col gap-2.5') }}>
     {{ $slot }}
 </div>
--- resources/views/components/menu/footer/item.blade.php
+++ resources/views/components/menu/footer/item.blade.php
@@ -1,6 +1,6 @@
 @props(['link'])
 
-<a {{ $attributes->class('text-charm-cream-100 text-lg font-[Poppins] flex gap-2.5 items-center group') }}
+<a {{ $attributes->class('text-charm-cream-100 text-lg font-[Poppins] flex gap-1.5 lg:gap-2.5 items-center group') }}
     href="{{ route($link) }}">
     <span
         class="size-3 bg-charm-cream-100 rotate-45 group-hover:rotate-90 transition-transform duration-300 block"></span>
